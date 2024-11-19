a05a1b36c530818d9a1ba3ba8eaab6cdff8e4052
Andrew Bell <acrew1905@gmail.com>
2024-11-19 00:05:55 -0500

    changes to login/out

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
 import LoginPanel from "./components/Login/Login"
+import Register_panel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register_panel />}> </Route>
     </Routes>
   );
 }
