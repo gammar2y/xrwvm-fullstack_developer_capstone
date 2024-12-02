2d544b60a9520add444a7c5f9bd86eb850556e6f
Andrew Bell <acrew1905@gmail.com>
2024-12-02 01:11:16 -0500

    ty

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,13 +2,16 @@ import LoginPanel from "./components/Login/Login"
 import Register_panel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview"
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register_panel />}> </Route>
       <Route path="/dealers" element={<Dealers/>} />
-    </Routes>
+      <Route path="/dealer/:id" element={<Dealer/>} />
+          </Routes>
   );
 }
 export default App;
