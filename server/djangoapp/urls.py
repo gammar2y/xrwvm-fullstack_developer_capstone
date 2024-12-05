a5d178dc590cd1c0a0b5f2b5c0f5d2050901693f
gammar2y <156562743+gammar2y@users.noreply.github.com>
2024-12-05 16:48:31 -0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,7 @@
-# Uncomment the imports before you add the code
+"""
+URL configuration for the djangoapp application.
+"""
+
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,20 +10,12 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     path(route='registration', view=views.registration, name='registration'),
-
     path(route='logout', view=views.logout, name='logout'),
-    
     path(route='login', view=views.login_user, name='login'),
-
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-
-    # path for dealer reviews view
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
